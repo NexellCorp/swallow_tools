78ab0c8946de47a64fef5f1c9dc2e0efbd06ab94
suker <suker@nexell.co.kr>
2018-03-23 13:06:22 +0900

    risc-v: tools: riscv-tools build script update
    This patch is about build script update for riscv-tools which include
    riscv-toolchains and gdb debugger.
    
    When using gdb, there is an issue where the breakpoint is not caught and the following message appears.
    
    Change-Id: I1b01afc9de3dfc9747efff2b8a128cc94dbab1d5
    Signed-off-by: suker <suker@nexell.co.kr>

--- riscv-tools-setup.sh
+++ riscv-tools-setup.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-mkdir -p ${HOME}/riscv-toolchain
+TOOLS_PATH=`dirname $0`
+ROOT_PATH=`readlink -ev ${TOOLS_PATH}/..`
 
-cd riscv-tools
+RISCV_TOOLS_PATH=`readlink -ev ${ROOT_PATH}/riscv-tools`
+RISCV_GDB_PATH=`readlink -ev ${RISCV_TOOLS_PATH}/riscv-gnu-toolchain/riscv-binutils-gdb`
+
+mkdir -p ${HOME}/riscv-toolchain
+TARGET_TOOLCHAIN_PATH=`readlink -ev ${HOME}/riscv-toolchain`
+
+
+cd ${RISCV_TOOLS_PATH}
 git submodule update --init --recursive
-export RISCV=${HOME}/riscv-toolchain/
+
+export RISCV=${TARGET_TOOLCHAIN_PATH}
 export PATH=$PATH:$RISCV/bin
+
+function patch_apply()
+{
+    #riscv-gnu-toolchain/riscv-binutils-gdb
+    cp ${RISCV_TOOLS_PATH}/patchfiles/gdb/* ${RISCV_GDB_PATH}/
+    pushd ${RISCV_GDB_PATH}
+    git checkout riscv-next
+    patch -p1 < *.patch
+    popd
+}
+
+patch_apply
 
 ./build.sh
